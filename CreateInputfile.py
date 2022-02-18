edee731e810ca797903ba5c298f5907862af0f8d
Caro1212 <carolinekries@gmx.de>
2022-02-18 18:29:18 +0100

    change2

--- CreateInputfile.py
+++ CreateInputfile.py
@@ -3,7 +3,7 @@ from subprocess import PIPE
 import time
 import os
 
-#change
+#change2
 path = 'C:/Users/Caroline Kries/sciebo/Forschungsprojekt Medikamenteninteraktion/04_Quellcode/02_Python/'
 
 os.chdir(path)
